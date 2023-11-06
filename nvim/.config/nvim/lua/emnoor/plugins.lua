fcc24a8d7705363e2ea4b0e64ce20a54c4bc6cae
Enam Mijbah Noor <enammijbahnoor@gmail.com>
2023-11-06 08:03:24 +0600

    Remove git signs from neovim

--- nvim/.config/nvim/lua/emnoor/plugins.lua
+++ nvim/.config/nvim/lua/emnoor/plugins.lua
@@ -15,19 +15,18 @@ require('lazy').setup({
   -- Git related plugins
   'tpope/vim-fugitive',
   --'tpope/vim-rhubarb',
-  { -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
+  -- { -- Adds git releated signs to the gutter, as well as utilities for managing changes
+  --   'lewis6991/gitsigns.nvim',
+  --   opts = {
+  --     signs = {
+  --       add = { text = '+' },
+  --       change = { text = '~' },
+  --       delete = { text = '_' },
+  --       topdelete = { text = '‾' },
+  --       changedelete = { text = '~' },
+  --     },
+  --   },
+  -- },
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
