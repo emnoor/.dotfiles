6acbcfd3141de54784cdcc3c54510f124da07652
Enam Mijbah Noor <enammijbahnoor@gmail.com>
2024-02-22 01:48:42 +0600

    Neovim: make lualine cooler :3

--- nvim/.config/nvim/lua/myplugins/init.lua
+++ nvim/.config/nvim/lua/myplugins/init.lua
@@ -63,10 +63,7 @@ return {
     'nvim-lualine/lualine.nvim',
     opts = {
       options = {
-        icons_enabled = false,
         theme = 'tokyonight',
-        component_separators = '|',
-        section_separators = '',
       },
     },
   },
